be582533227356472904062a4eb91b8850dc6018
Subham Jaiswal <subham.jaiswal@zennioptical.com>
2022-10-18 13:18:13 +0530

    Update metro-config.js

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,5 @@
+const { getDefaultConfig } = require("metro-config");
+
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
   const { transformer, resolver } = config;
