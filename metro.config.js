8bc9a10b65de40cda552526b55f4dac117cf983e
Subham Jaiswal <subham.jaiswal@zennioptical.com>
2022-10-18 13:32:06 +0530

    Scheme added and Updated Metro Config

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,20 @@
 const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
-  const { transformer, resolver } = config;
-
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("./transformersBundle.js"),
+  const {
+    resolver: { assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: [...assetExts, "cjs"],
+    },
   };
-
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter(
-      (ext) => ext !== "svg" && ext !== "scss"
-    ),
-    sourceExts: [...resolver.sourceExts, "svg", "scss", "sass"],
-  };
-
-  return config;
 })();
